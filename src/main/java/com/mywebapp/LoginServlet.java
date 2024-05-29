05325e765adc1658e18a591138cb4858b7637706
venkatreddy <mvsateeshqa@gmail.com>
2024-05-29 20:34:06 +0530

    first commit

---
+++ src/main/java/com/mywebapp/LoginServlet.java
@@ -0,0 +1,23 @@
+package com.mywebapp;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+    private static final String VALID_USERNAME = "Admin@123";
+    private static final String VALID_PASSWORD = "Servlet@123";
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("Username");
+        String password = request.getParameter("Password");
+
+        if (VALID_USERNAME.equals(username) && VALID_PASSWORD.equals(password)) {
+            request.getRequestDispatcher("WelcomeServlet").forward(request, response);
+        } else {
+            response.setContentType("text/html");
+            response.getWriter().println("Sorry, Username or Password Wrong");
+        }
+    }
+}
